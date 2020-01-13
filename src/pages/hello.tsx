cf2762161671027c7fa3892a8e713ced03d89fee
seonghyeon <impressor7@gmail.com>
2020-01-13 23:28:41 +0900

    update hello page to have getInitialProps

--- src/pages/hello.tsx
+++ src/pages/hello.tsx
@@ -1,12 +1,23 @@
+import axios from 'axios';
+import React from 'react';
 import Head from 'next/head';
 
-const Page = () => {
-  return (
-    <>
-      <Head><title>HelloPage</title></Head>
-      <div>Welcome Hello page</div>
-    </>
-  );
+type Props = { name: string; };
+class Page extends React.Component<Props> {
+  static async getInitialProps(ctx) {
+    const { data } = await axios.get('/api/hello-world');
+    return data;
+  }
+
+  render() {
+    const { name } = this.props;
+    return (
+      <>
+        <Head><title>HelloPage</title></Head>
+        <div>Welcome, {name}</div>
+      </>
+    )
+  }
 }
 
 export default Page;
